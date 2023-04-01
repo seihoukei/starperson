08638f0c578b794c690993dea081d95ddf845dae
seihoukei <a@zefiris.su>
2023-04-01 20:58:35 +0300

    Progress bar

---
+++ src/components/ui/UIProgressBar.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let max = 100
+    export let current = 0
+    export let caption = ""
+
+    $: progress = (100 * current / max).toFixed(2)
+</script>
+
+<div class="bar" style="--progress:{progress}%">
+    <div class="bg"></div>
+    <div class="fg"></div>
+    <div class="caption">{caption}</div>
+</div>
+
+<style>
+    div.bar {
+        width: 16em;
+        height: 0.5em;
+        padding: 10px 0;
+        position: relative;
+    }
+
+    div.fg, div.bg, div.caption {
+        position: absolute;
+        left : 0;
+        top : 0;
+        height : 100%;
+        width: 100%;
+    }
+
+    div.bg {
+        background-color: #222222;
+    }
+
+    div.fg {
+        background-color: #666666;
+        width: var(--progress);
+    }
+
+    div.caption {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
